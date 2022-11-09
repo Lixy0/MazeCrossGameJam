427074920a165e2db85628edb633b6b7bbfd3e7d
Lixy0 <lixygame@gmail.com>
2022-11-09 14:16:41 +0100

    code wall-problem move

--- MazeMaze/Assets/Script/playerController.cs
+++ MazeMaze/Assets/Script/playerController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class playerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    public bool canMove = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,29 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (canMove == true)
+        {
+            // player input
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+
+            // move player
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            canMove = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision2D collision)
+    {
+        canMove = false;
     }
 }
